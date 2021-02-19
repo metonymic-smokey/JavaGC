91f9a05acf1cf321068a5bfc836b54bdd0d9c4c3
metonymic-smokey <ahuja.aditi@gmail.com>
2021-02-19 18:05:56 +0530

    JMH benchmark for heap memroy bandwidth allocator

--- IonutBench/HeapMemoryBandwidthAllocatorBenchmark/HMBAB.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-n=$1
-
-javac HeapMemoryBandwidthAllocatorBenchmark.java 
-java HeapMemoryBandwidthAllocatorBenchmark $n 
-jar cmvf META-INF/MANIFEST.MF HeapMemoryBandwidthAllocatorBenchmark.jar HeapMemoryBandwidthAllocatorBenchmark.class
-java  -Xlog:gc*:file=trace.log -jar HeapMemoryBandwidthAllocatorBenchmark.jar $n 
