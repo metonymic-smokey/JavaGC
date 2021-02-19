ef4b23b8c2ad4afee0b99eb9f05a301686a11972
metonymic-smokey <ahuja.aditi@gmail.com>
2021-02-19 17:52:00 +0530

    added JMH benchmark to write barriers benchmark

---
+++ IonutBench/WriteBarriersLoopingOverArrayBenchmark/src/main/java/com/avenuecode/snippet/MyBenchmark.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.avenuecode.snippet;
+
+import java.util.ArrayList;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+public class MyBenchmark {
+
+    @State(Scope.Benchmark)
+    public static class BenchmarkState {
+        int size,lRefInteger;
+        int[] array;
+
+    }
+
+    @Benchmark
+    public void testMethod(BenchmarkState state) {
+        for (int i = 0; i < state.size; i++) {
+            state.array[i] = state.lRefInteger;
+        }
+
+    }
+
+    public static void main(String[] args) {
+       BenchmarkState b = new BenchmarkState();
+       b.size = Integer.parseInt(args[0]);
+       b.lRefInteger = Integer.parseInt(args[1]);
+       b.array = new int[b.size];
+       
+    }
+
+}
