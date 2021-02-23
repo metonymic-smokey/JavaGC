2c53166d682f6bad8171c665da9a9751b0e6548e
metonymic-smokey <ahuja.aditi@gmail.com>
2021-02-23 21:32:12 +0530

    minor fix

--- IonutBench/BurstHeapMemoryAllocatorBenchmark/run.sh
+++ IonutBench/BurstHeapMemoryAllocatorBenchmark/run.sh
@@ -8,7 +8,7 @@ Type=("Serial" "ParallelOld" "ConcMarkSweep" "G1" "Shenandoah" "Z" "Epsilon")
 for t in ${Type[@]}; do
   echo $t
   GC="Use"$t"GC"
-  java -XX:+UnlockDiagnosticVMOptions -Xms4g -Xmx4g -XX:+AlwaysPreTouch -XX:+$GC -jar target/benchmarks.jar> Results/$t.log
+  java -XX:+UnlockExperimentalVMOptions -Xms4g -Xmx4g -XX:+AlwaysPreTouch -XX:+$GC -jar target/benchmarks.jar> Results/$t.log
   
 
 done
