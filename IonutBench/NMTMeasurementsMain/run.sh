48d3f632fd1ed0341c73648efceb6d207d5a3acd
Samyak S Sarnayak <samyak201@gmail.com>
2021-03-09 23:18:27 +0530

    Ionut - NMT - fix script to correctly place the GC parameter

--- IonutBench/NMTMeasurementsMain/run.sh
+++ IonutBench/NMTMeasurementsMain/run.sh
@@ -5,8 +5,8 @@ Type=("Serial" "ParallelOld" "ConcMarkSweep" "G1" "Shenandoah" "Z" "Epsilon")
 for t in ${Type[@]}; do
   echo $t
   GC="Use"$t"GC"
-  java -XX:+UnlockDiagnosticVMOptions -XX:+PrintNMTStatistics -XX:NativeMemoryTracking=summary -Xms4g -Xmx4g -XX:+AlwaysPreTouch NMTMeasurementsMain -XX:+$GC > $t.log
-  
+  java -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions -XX:+$GC -XX:+PrintNMTStatistics -XX:NativeMemoryTracking=summary -Xms4g -Xmx4g -XX:+AlwaysPreTouch NMTMeasurementsMain > $t.log
+
 
 done
 
