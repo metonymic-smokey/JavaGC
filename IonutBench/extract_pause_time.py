dd29079a7114586e3d5bbb1492a58b40c04552e5
Samyak S Sarnayak <samyak201@gmail.com>
2021-03-09 19:23:09 +0530

    logs - add data for Parallel and Serial

--- IonutBench/extract_pause_time.py
+++ IonutBench/extract_pause_time.py
@@ -23,6 +23,7 @@ outcsv = csv.writer(outfile)
 # CMS GC
 total_young: List[Union[int, float]] = [0, 0, 0]
 total_old: List[Union[int, float]] = [0, 0, 0]
+total_count = 0
 for fil in d["gc_ConcMarkSweep"]:
     young_gc_re = re.compile(
         r"young generation GC time (\d+\.\d+) secs, (\d+) GC's, avg GC time (\d+\.\d+)"
@@ -44,6 +45,7 @@ for fil in d["gc_ConcMarkSweep"]:
     total_old[0] += float(matc.group(1))
     total_old[1] += int(matc.group(2))
     total_old[2] += float(matc.group(3))
+    total_count += 1
 
 outcsv.writerow(
     [
@@ -54,6 +56,7 @@ outcsv.writerow(
         "Old total time",
         "No. of old GC runs",
         "Avg old GC time",
+        "Log count"
     ]
 )
 pprint(total_young)
@@ -62,17 +65,103 @@ num = len(d["gc_ConcMarkSweep"])
 outcsv.writerow(
     [
         "ConcMarkSweep",
-        total_young[0] / num,
-        total_young[1] // num,
-        total_young[2] / num,
-        total_old[0] / num,
-        total_old[1] // num,
-        total_old[1] / num,
+        total_young[0] / total_count,
+        total_young[1] // total_count,
+        total_young[2] / total_count,
+        total_old[0] / total_count,
+        total_old[1] // total_count,
+        total_old[1] / total_count,
+        total_count,
+
     ]
 )
 outcsv.writerow([])
 
 # for gc_name, fils in d.items():
 #     for fil in fils:
+
+# Parallel Old
+total_young: List[float] = [0]
+total_old: List[float] = [0]
+total_count = 0
+for fil in d["gc_ParallelOld"]:
+    young_gc_re = re.compile(
+        r"young generation GC time (\d+\.\d+) secs"
+    )
+    old_gc_re = re.compile(
+        r"old generation GC time (\d+\.\d+) secs"
+    )
+    with open(os.path.join(log_dir, fil), "rt") as f:
+        last_2_lines = f.readlines()[-2::1]
+
+    matc = young_gc_re.search(last_2_lines[0])
+    if matc is None:
+        continue
+    # print(matc.group(1), matc.group(2), matc.group(3))
+    total_young[0] += float(matc.group(1))
+
+    matc = old_gc_re.search(last_2_lines[1])
+    # print(matc.group(1), matc.group(2), matc.group(3))
+    total_old[0] += float(matc.group(1))
+    total_count += 1
+pprint(total_young)
+pprint(total_old)
+outcsv.writerow(
+    [
+        "ParallelOld",
+        total_young[0] / total_count,
+        None,
+        None,
+        total_old[0] / total_count,
+        None,
+        None,
+        total_count
+    ]
+)
+outcsv.writerow([])
+
+# Serial GC
+total_young: List[Union[int, float]] = [0, 0, 0]
+total_old: List[Union[int, float]] = [0, 0, 0]
+total_count = 0
+for fil in d["gc_Serial"]:
+    young_gc_re = re.compile(
+        r"young generation GC time (\d+\.\d+) secs, (\d+) GC's, avg GC time (\d+\.\d+)"
+    )
+    old_gc_re = re.compile(
+        r"old generation GC time (\d+\.\d+) secs, (\d+) GC's, avg GC time (\d+\.\d+)"
+    )
+    with open(os.path.join(log_dir, fil), "rt") as f:
+        last_2_lines = f.readlines()[-2::1]
+
+    matc = young_gc_re.search(last_2_lines[0])
+    # print(matc.group(1), matc.group(2), matc.group(3))
+    total_young[0] += float(matc.group(1))
+    total_young[1] += int(matc.group(2))
+    total_young[2] += float(matc.group(3))
+
+    matc = old_gc_re.search(last_2_lines[1])
+    print(matc.group(1), matc.group(2), matc.group(3))
+    total_old[0] += float(matc.group(1))
+    total_old[1] += int(matc.group(2))
+    total_old[2] += float(matc.group(3))
+    total_count += 1
+
+pprint(total_young)
+pprint(total_old)
+outcsv.writerow(
+    [
+        "Serial",
+        total_young[0] / total_count,
+        total_young[1] // total_count,
+        total_young[2] / total_count,
+        total_old[0] / total_count,
+        total_old[1] // total_count,
+        total_old[1] / total_count,
+        total_count,
+
+    ]
+)
+outcsv.writerow([])
 
 outfile.close()
