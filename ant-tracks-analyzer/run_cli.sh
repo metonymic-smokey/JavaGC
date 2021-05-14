e4fcece3a9f924ea56efbbb50ec372bac58fa1b9
Samyak S Sarnayak <samyak201@gmail.com>
2021-05-14 11:22:52 +0530

    add project and classpath files

--- ant-tracks-analyzer/run_cli.sh
+++ ant-tracks-analyzer/run_cli.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar -Dat.jku.anttracks.gui.printStatisticsPath="stats.txt" Tool/Parser/build/libs/Parser.jar $1 ReportGC=true UseCallContext=true
+java -XX:+PrintGCDetails -XX:+PrintGC -Xloggc:gc.log -jar -Dat.jku.anttracks.gui.printStatisticsPath="stats.txt" Tool/CLI/build/libs/CLI.jar $1 ReportGC=true UseCallContext=true
