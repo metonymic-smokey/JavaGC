169a401698cfd51de0d49bc4f671b5e06357632e
Samyak S Sarnayak <samyak201@gmail.com>
2021-08-15 03:41:44 -0700

    add Markus' code
    JsonExportMain and the TemplateMain

--- ant-tracks-analyzer/Tool/CLI/src/main/java/com/brr/anttracks/cli/main/JsonExportMain.java
+++ ant-tracks-analyzer/Tool/CLI/src/main/java/com/brr/anttracks/cli/main/JsonExportMain.java
@@ -1,9 +1,14 @@
 package com.brr.anttracks.cli.main;
 
+import at.jku.anttracks.classification.ClassifierChain;
+import at.jku.anttracks.classification.Filter;
+import at.jku.anttracks.classification.trees.ListClassificationTree;
+import at.jku.anttracks.gui.classification.classifier.AllocationSiteClassifier;
+import at.jku.anttracks.gui.classification.classifier.CallSitesClassifier;
+import at.jku.anttracks.gui.classification.classifier.TypeClassifier;
 import at.jku.anttracks.gui.model.AppInfo;
 import at.jku.anttracks.gui.utils.Consts;
-import at.jku.anttracks.heap.DetailedHeap;
-import at.jku.anttracks.heap.HeapListener;
+import at.jku.anttracks.heap.*;
 import at.jku.anttracks.heap.io.MetaDataWriterConfig;
 import at.jku.anttracks.heap.objects.ObjectInfo;
 import at.jku.anttracks.heap.roots.RootPtr;
@@ -24,15 +29,23 @@ import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.LogManager;
 
 // Provided by Markus Weninger, SSW, JKU, Austria
-public class Main {
+public class JsonExportMain {
 
     private static AppInfo appInfo;
     private static Symbols symbols;
     private static HeapTraceParser parser;
+
+    private static List<GarbageCollectionCause> gcCauses = new ArrayList<>();
+    private static List<GarbageCollectionType> gcTypes = new ArrayList<>();
+    private static List<Long> gcStarts = new ArrayList<>();
+    private static List<Long> gcEnds = new ArrayList<>();
 
     public static void main(String[] args) {
         // Turn off other logging (e.g., AdditionalPrintingEventHandler)
@@ -41,6 +54,7 @@ public class Main {
         int metaDumpCount = MetaDataWriterConfig.DEFAULT_DUMP_COUNT;
         boolean metaMajorGCsOnly = MetaDataWriterConfig.DEFAULT_AT_MAJOR_GCS_ONLY;
         boolean check = false;
+
 
         if (args.length == 0) {
             System.err.println("No trace file path given!");
@@ -51,7 +65,7 @@ public class Main {
         try {
             // AppInfo represents information about the currently analyzed application
             // This would not be ultimately necessary but make working with classification a bit easier
-            appInfo = new AppInfo(traceFile.getAbsolutePath(), traceFile, null);
+            appInfo = new AppInfo(traceFile.getAbsolutePath().toString(), traceFile, null);
             // The MetaDataPath is where statistics, etc. are going to be stored while the trace is parsed
             appInfo.setMetaDataPath((BaseFile.isPlainFile(SymbolsFile.SYMBOL_FILE_ID, appInfo.getSymbolsFile()) ? appInfo.getSymbolsFile().getParent() : appInfo.getSymbolsFile()).toString() + File.separator + Consts.ANT_META_DIRECTORY);
 
@@ -65,16 +79,15 @@ public class Main {
 
             // Then, create a trace parser that uses this symbols information
             parser = new HeapTraceParser(appInfo.getSymbols());
-            // If we want, we can add a meta data writer to store statistics (e.g., heap size, etc.) to disk
-            // This also writes certain heap states to disk to make subsequent analyses of the same trace file faster
-            // Since this is not needed, we disable it at the moment
-            // parser.addHeapListener(new MetaDataWriterListener(new MetaDataWriterConfig(appInfo.getMetaDataPath()), Statistics.Companion::collect));
+            // Once the whole trace has been parsed, if meta-data has been written, we can read statistics information.
+            // Some analyses then be performed based on the statistics stored in appInfo.getStatistics()
+            // appInfo.getStatistics().addAll(Statistics.Companion.readStatisticsFromMetadata(appInfo.getMetaDataPath(), appInfo.getSymbols()));
 
             // TODO: Modify the method customHeapListener if you want to perform tasks at the start and at the end of GCs (see the TODOs in this method)
             parser.addHeapListener(customHeapListener());
             // TODO Modify the method customEventHandler if you want to inspect each event read from the trace file.
             // For example, this could be used to count the number of allocation events, move events, etc.
-            parser.addEventHandler(Main::customEventHandler);
+            parser.addEventHandler(JsonExportMain::customEventHandler);
             DetailedHeap detailedHeap = parser.parse();
             // Once the whole trace has been parsed, if meta-data has been written, we can read statistics information.
             // Some analyses then be performed based on the statistics stored in appInfo.getStatistics()
@@ -95,6 +108,10 @@ public class Main {
                 DetailedHeap heap = (DetailedHeap) sender;
                 // TODO: Optionally add own code that should be executed at the very end
                 System.out.println("FINISHED");
+                System.out.println(gcCauses);
+                System.out.println(gcTypes);
+                System.out.println(gcStarts);
+                System.out.println(gcEnds);
             }
 
             @Override
@@ -107,6 +124,10 @@ public class Main {
                     // TODO: Do something before the GC phase starts
                     // If you want to inspect the heap, we suggest to use IndexBasedHeap idxHeap = heap.toIndexBasedHeap(false, null) for faster object access
                     System.out.printf("GC %d starts (mutator phase was running from %,dms to %,dms)%n", to.getId(), from.getTime(), to.getTime());
+                    gcCauses.add(to.getCause());
+                    gcTypes.add(to.getType());
+                    gcStarts.add(to.getTime());
+                    groupAndWrite(heap, to.getTime());
                 }
             }
 
@@ -120,9 +141,42 @@ public class Main {
                     // TODO: Do something after the GC phase is over
                     // If you want to inspect the heap, we suggest to use IndexBasedHeap idxHeap = heap.toIndexBasedHeap(false, null) for faster object access
                     System.out.printf("GC %d ends (GC phase was running from %,dms to %,dms)%n", to.getId(), from.getTime(), to.getTime());
+                    gcEnds.add(to.getTime());
+                    groupAndWrite(heap, to.getTime());
                 }
             }
         };
+    }
+
+    private static void groupAndWrite(DetailedHeap heap, long time) {
+        // An IndexBasedHeap allows us to access heap objects via an index
+        // This makes access times faster and helps us to faster build classification trees
+        IndexBasedHeap idxHeap = heap.toIndexBasedHeap(false, null);
+        // Filters could be used to exclude certain objects from the classification tree
+        Filter[] filters = new Filter[0];
+        TypeClassifier classifier1 = new TypeClassifier();
+        AllocationSiteClassifier classifier2 = new AllocationSiteClassifier();
+        CallSitesClassifier classifier3 = new CallSitesClassifier();
+        // classifier.setup() calls are not needed, groupListParallel takes care of that :)
+        // Just combine all classifiers to a ClassifierChain
+        ClassifierChain classifiers = new ClassifierChain(classifier1, classifier2, classifier3);
+        // Finally, group the objects in the heap into a tree
+        ListClassificationTree tree = idxHeap.groupListParallel(filters, classifiers, true, true, null, null);
+        // Init some statistic fields in the tree
+        // If you want the have info about the deep size (reachabiltiy) and retained size (ownership), set the first two boolean flags to true
+        tree.init(idxHeap, false, false, false, false);
+        if (!Files.exists(Paths.get("./outputs"))) {
+            try {
+                Files.createDirectory(Paths.get("./outputs"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        tree.getRoot().exportAsJSON(idxHeap,
+                classifiers,
+                time,
+                new File("outputs/heapstate_withClassifierIdPerNode_" + time + ".json"),
+                new File("outputs/heapstate_withClassifierNamePerNode_" + time + ".json"));
     }
 
     // TODO Modify this method if you want to inspect the events read from the trace file (e.g., counting ObjAlloc events)
