13672d8c0d1f2af21cbeff9f3835ef0f5ea140b2
Aditi Ahuja <48997495+metonymic-smokey@users.noreply.github.com>
2021-09-03 08:32:43 +0530

    Add average lifespan script

---
+++ ant-tracks-analyzer/analysis/lifespan.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import itertools
+from tqdm.notebook import tqdm
+
+df = pd.read_csv("objects_17.txt.csv")
+
+types = df["type"].unique()
+
+df_objs = dict()
+
+for type in types:
+    df_objs[type] = df[df["type"] == type]
+    
+    stack = []
+    c=0
+    total=0
+
+    for index,row in df_objs[type].iterrows():
+        #print(row['event'])
+        if row['event'] == "BORN":
+            stack.append(row['at'])
+
+        if row['event'] == "YEETED":
+            #died = max(died,row['at'])
+            if len(stack) > 0:
+                born = stack.pop()
+                diff = row['at'] - born
+                total+=diff
+
+        c+=1
+
+    print(type,total/c)
+
+
