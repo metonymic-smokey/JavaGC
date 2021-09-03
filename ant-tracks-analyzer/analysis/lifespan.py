9a9225c94402acf8b4e6b1a3742107bfe64f15d9
Aditi Ahuja <48997495+metonymic-smokey@users.noreply.github.com>
2021-09-03 14:27:40 +0530

    grouped based on tags

--- ant-tracks-analyzer/analysis/lifespan.py
+++ ant-tracks-analyzer/analysis/lifespan.py
@@ -10,24 +10,42 @@ df_objs = dict()
 
 for type in types:
     df_objs[type] = df[df["type"] == type]
-    
+
     stack = []
     c=0
     total=0
 
-    for index,row in df_objs[type].iterrows():
-        #print(row['event'])
-        if row['event'] == "BORN":
-            stack.append(row['at'])
-            c+=1
+    tags = df_objs[type]["tag"].unique()
+    print(tags)
 
-        if row['event'] == "YEETED":
-            if len(stack) > 0:
-                born = stack.pop()
-                diff = row['at'] - born
-                total+=diff
+    temp = df_objs[type]
+
+    for tag in tags:
+        tag_objs = dict()
+        if tag not in tag_objs:
+            tag_objs[tag] = []
+
+        for index,row in df_objs[type].iterrows():
+            if row["tag"] == tag:
+                tag_objs[tag].append(row)
+
+        for k in tag_objs.keys():
+            tag_objs[k] = pd.DataFrame(tag_objs[k])
+
+        for index,row in tag_objs[tag].iterrows():
+
+            if row['event'] == "BORN":
+                stack.append(row['at'])
                 c+=1
 
-    print(type,total/c)
+            if row['event'] == "YEETED":
+                if len(stack) > 0:
+                    born = stack.pop()
+                    diff = row['at'] - born
+                    total+=diff
+                    c+=1
+
+    
+            print(type,tag,total/c)
 
 
