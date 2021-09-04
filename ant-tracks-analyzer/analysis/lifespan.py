51c5e8ffb6da284ebe7d29844dbce57360b8994f
Samyak S Sarnayak <samyak201@gmail.com>
2021-09-04 13:47:52 +0530

    format lifespan script with black, limit to 1000 rows
    and print only average per type (class) of object, not for every tag

--- ant-tracks-analyzer/analysis/lifespan.py
+++ ant-tracks-analyzer/analysis/lifespan.py
@@ -1,19 +1,25 @@
-import pandas as pd
-import itertools
-from tqdm.notebook import tqdm
+import time
 
-df = pd.read_csv("objects_17.txt.csv")
+import pandas as pd
+
+df = pd.read_csv("preprocessed_outputs/objects_14.txt.csv")
+
+df = df[:1000]
 
 types = df["type"].unique()
 
+print(types)
+
 df_objs = dict()
+
+start = time.time()
 
 for type in types:
     df_objs[type] = df[df["type"] == type]
 
     stack = []
-    c=0
-    total=0
+    c = 0
+    total = 0
 
     tags = df_objs[type]["tag"].unique()
     print(tags)
@@ -25,27 +31,27 @@ for type in types:
         if tag not in tag_objs:
             tag_objs[tag] = []
 
-        for index,row in df_objs[type].iterrows():
+        for index, row in df_objs[type].iterrows():
             if row["tag"] == tag:
                 tag_objs[tag].append(row)
 
         for k in tag_objs.keys():
             tag_objs[k] = pd.DataFrame(tag_objs[k])
 
-        for index,row in tag_objs[tag].iterrows():
+        for index, row in tag_objs[tag].iterrows():
 
-            if row['event'] == "BORN":
-                stack.append(row['at'])
-                c+=1
+            if row["event"] == "BORN":
+                stack.append(row["at"])
+                c += 1
 
-            if row['event'] == "YEETED":
+            if row["event"] == "YEETED":
                 if len(stack) > 0:
                     born = stack.pop()
-                    diff = row['at'] - born
-                    total+=diff
-                    c+=1
+                    diff = row["at"] - born
+                    total += diff
+                    c += 1
 
-    
-            print(type,tag,total/c)
+    print(type, total / c)
 
 
+print("Time taken: ", time.time() - start)
