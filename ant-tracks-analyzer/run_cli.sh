ed1efffe524bfd2db3ace12c72b6e9c409c19658
Samyak S Sarnayak <samyak201@gmail.com>
2021-10-23 08:10:34 -0700

    fix: do not trace GC in CLI

--- ant-tracks-analyzer/run_cli.sh
+++ ant-tracks-analyzer/run_cli.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -XX:+PrintGCDetails -XX:+PrintGC -Xloggc:gc.log -jar -Dat.jku.anttracks.gui.printStatisticsPath="stats.txt" Tool/CLI/build/libs/CLI.jar $1 ReportGC=true UseCallContext=true
+# java -XX:+PrintGCDetails -XX:+PrintGC -Xloggc:gc.log -jar -Dat.jku.anttracks.gui.printStatisticsPath="stats.txt" Tool/CLI/build/libs/CLI.jar $1 ReportGC=true UseCallContext=true
+java -jar -Dat.jku.anttracks.gui.printStatisticsPath="stats.txt" Tool/CLI/build/libs/CLI.jar $1 ReportGC=true UseCallContext=true
